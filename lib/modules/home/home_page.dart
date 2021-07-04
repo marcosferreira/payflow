e9c198bf17998b3e299a62c81076aebea7ca2c3d
Marcos Ferreira <marcosferreira.developer@gmail.com>
2021-07-04 07:49:48 -0300

    create HomeController add BottomNavigatorBar

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/home/home_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+  final pages = [
+    Container(color: Colors.red),
+    Container(color: Colors.blue),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -15,9 +27,15 @@ class HomePage extends StatelessWidget {
           color: AppColors.primary,
           child: Center(
             child: ListTile(
-              title: Text(
-                'Olá Marcos',
-                style: TextStyles.titleRegular,
+              title: Text.rich(
+                TextSpan(
+                  text: 'Olá, ',
+                  style: TextStyles.titleRegular,
+                  children: [
+                    TextSpan(
+                        text: 'Marcos', style: TextStyles.titleBoldBackground)
+                  ],
+                ),
               ),
               subtitle: Text(
                 'Mantenha suas contas em dia',
@@ -33,6 +51,50 @@ class HomePage extends StatelessWidget {
               ),
             ),
           ),
+        ),
+      ),
+      body: pages[controller.currentPage],
+      bottomNavigationBar: Container(
+        height: 90,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+                onPressed: () {
+                  controller.setPage(0);
+                  setState(() {});
+                },
+                icon: Icon(
+                  Icons.home,
+                  color: AppColors.primary,
+                )),
+            GestureDetector(
+              onTap: () {
+                print('Clicou');
+              },
+              child: Container(
+                width: 56,
+                height: 56,
+                decoration: BoxDecoration(
+                  color: AppColors.primary,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Icon(
+                  Icons.add_box_outlined,
+                  color: AppColors.background,
+                ),
+              ),
+            ),
+            IconButton(
+                onPressed: () {
+                  controller.setPage(1);
+                  setState(() {});
+                },
+                icon: Icon(
+                  Icons.description_outlined,
+                  color: AppColors.body,
+                )),
+          ],
         ),
       ),
     );
