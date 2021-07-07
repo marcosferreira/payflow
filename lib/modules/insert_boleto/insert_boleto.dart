0955428605bd4234ba3e4de6ea0a6c7b2438c95b
Marcos Ferreira <marcosferreira.developer@gmail.com>
2021-07-07 06:03:52 -0300

    fixing layout insert_boleto_page

--- lib/modules/insert_boleto/insert_boleto.dart
+++ lib/modules/insert_boleto/insert_boleto.dart
@@ -3,6 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
 import 'package:payflow/shared/widget/input_text/input_text_widget.dart';
+import 'package:payflow/shared/widget/set_label_buttons/set_label_buttons.dart';
 
 class InsertBoletoPage extends StatelessWidget {
   const InsertBoletoPage({Key? key}) : super(key: key);
@@ -19,38 +20,48 @@ class InsertBoletoPage extends StatelessWidget {
           color: AppColors.input,
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 93, vertical: 24),
-            child: Text(
-              'Preencha os dados do boleto',
-              style: TextStyles.titleBoldHeading,
-              textAlign: TextAlign.center,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 93, vertical: 24),
+              child: Text(
+                'Preencha os dados do boleto',
+                style: TextStyles.titleBoldHeading,
+                textAlign: TextAlign.center,
+              ),
             ),
-          ),
-          InputTextWidget(
-            label: 'Nome do boleto',
-            icon: Icons.description_rounded,
-            onChanged: (value) => {},
-          ),
-          InputTextWidget(
-            label: 'Vencimento',
-            icon: FontAwesomeIcons.timesCircle,
-            onChanged: (value) => {},
-          ),
-          InputTextWidget(
-            label: 'Valor',
-            icon: FontAwesomeIcons.wallet,
-            onChanged: (value) => {},
-          ),
-          InputTextWidget(
-            label: 'Código',
-            icon: FontAwesomeIcons.barcode,
-            onChanged: (value) => {},
-          ),
-        ],
+            InputTextWidget(
+              label: 'Nome do boleto',
+              icon: Icons.description_rounded,
+              onChanged: (value) => {},
+            ),
+            InputTextWidget(
+              label: 'Vencimento',
+              icon: FontAwesomeIcons.timesCircle,
+              onChanged: (value) => {},
+            ),
+            InputTextWidget(
+              label: 'Valor',
+              icon: FontAwesomeIcons.wallet,
+              onChanged: (value) => {},
+            ),
+            InputTextWidget(
+              label: 'Código',
+              icon: FontAwesomeIcons.barcode,
+              onChanged: (value) => {},
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: SetLabelButtons(
+        primaryLabel: 'Cancelar',
+        primaryOnPressed: () => {},
+        enableSecondaryColor: true,
+        secondaryLabel: 'Cadastrar',
+        secondaryOnPressed: () => {},
       ),
     );
   }
