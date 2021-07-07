4170064b647776240fec9796ac8ea43a2da2effb
Marcos Ferreira <marcosferreira.developer@gmail.com>
2021-07-07 05:50:04 -0300

    create InputTextWidget

--- lib/modules/insert_boleto/insert_boleto.dart
+++ lib/modules/insert_boleto/insert_boleto.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widget/input_text/input_text_widget.dart';
 
 class InsertBoletoPage extends StatelessWidget {
   const InsertBoletoPage({Key? key}) : super(key: key);
@@ -6,8 +10,47 @@ class InsertBoletoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.background,
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: Text('Insert Boleto Page'),
+        backgroundColor: AppColors.background,
+        elevation: 0,
+        leading: BackButton(
+          color: AppColors.input,
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 93, vertical: 24),
+            child: Text(
+              'Preencha os dados do boleto',
+              style: TextStyles.titleBoldHeading,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          InputTextWidget(
+            label: 'Nome do boleto',
+            icon: Icons.description_rounded,
+            onChanged: (value) => {},
+          ),
+          InputTextWidget(
+            label: 'Vencimento',
+            icon: FontAwesomeIcons.timesCircle,
+            onChanged: (value) => {},
+          ),
+          InputTextWidget(
+            label: 'Valor',
+            icon: FontAwesomeIcons.wallet,
+            onChanged: (value) => {},
+          ),
+          InputTextWidget(
+            label: 'Código',
+            icon: FontAwesomeIcons.barcode,
+            onChanged: (value) => {},
+          ),
+        ],
       ),
     );
   }
